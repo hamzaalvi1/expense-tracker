8def8d298b6fd359ef774655a915bc98d6a8e8d5
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2024-05-13 20:17:06 +0500

    authentication added

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,15 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUpPage() {
+  return (
+    <SignUp
+      appearance={{
+        elements: {
+          headerTitle: "capitalize text-[22px] font-semibold",
+          button: " py-3 ",
+        },
+      }}
+      path="/sign-up"
+    />
+  );
+}
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/sign-up", "/sign-in"],
+});
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
